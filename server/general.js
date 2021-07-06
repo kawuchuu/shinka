83a64f6db62c20701d78dba31e9e66ad523336d5
kawuchuu <kawuchuu@users.noreply.github.com>
2021-07-07 01:14:35 +1000

    updated api for status dash

--- server/general.js
+++ server/general.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const { io } = require('.');
 const { bot } = require('../main')
 
 router.get('/bot', (ref, res) => {
-    res.send({
+    let botInfo = {
         username: bot.user.username,
         discriminator: bot.user.discriminator,
         id: bot.user.id,
@@ -10,14 +11,38 @@ router.get('/bot', (ref, res) => {
         createdAt: bot.user.createdTimestamp,
         avatar: bot.user.avatar,
         isPublic: bot.application.botPublic,
+        status: bot.user.presence.status,
         owner: {
             username: bot.application.owner.username,
             discriminator: bot.application.owner.discriminator,
             avatar: bot.application.owner.avatar,
             id: bot.application.owner.id,
-            mfaEnabled: bot.user.mfaEnabled
+            mfaEnabled: true
         }
-    })
+    }
+    if (bot.user.presence.activities) {
+        botInfo['activity'] = {
+            name: bot.user.presence.activities[0].name,
+            type: bot.user.presence.activities[0].type
+        }
+    }
+    res.send(botInfo)
+})
+
+bot.on('presenceUpdate', (oldP, newP) => {
+    if (newP.userID !== bot.user.id) return
+    let presence = {
+        status: newP.status
+    }
+    if (newP.activities[0]) {
+        presence['activities'] = {
+            details: newP.activities[0].details,
+            name: newP.activities[0].name,
+            state: newP.activities[0].state,
+            type: newP.activities[0].type
+        }
+    }
+    io.emit('presenceUpdate', presence)
 })
 
 module.exports = router
