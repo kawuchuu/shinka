29a4ea635d4112f61340ae7c69be36b9bbcc455c
kawuchuu <kawuchuu@users.noreply.github.com>
2021-08-07 14:48:59 +1000

    replace ytdl-core with youtube-dl-exec for stability

--- modules/yt/player.js
+++ modules/yt/player.js
@@ -1,15 +1,34 @@
-const ytdl = require('ytdl-core');
+const { raw } = require('youtube-dl-exec');
 //const { bot } = require('../../main')
-const { getVoiceConnection, createAudioPlayer, createAudioResource } = require('@discordjs/voice')
+const { getVoiceConnection, createAudioPlayer, createAudioResource, demuxProbe } = require('@discordjs/voice')
 let serverQueue = {np: null};
 
 const play = (msg) => {
     const server = serverQueue[msg.guild.id]
     const connection = getVoiceConnection(msg.guild.id)
     const player = createAudioPlayer()
-    let resource = createAudioResource(ytdl(server.queue[0].url))
-    player.play(resource)
-    connection.subscribe(player)
+    player.on('error', err => {
+        console.error(err)
+    })
+    const streamProcess = raw(server.queue[0].url, {
+        o: '-',
+        q: '',
+        f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
+        r: '100K'
+    })
+    const stream = streamProcess.stdout
+    streamProcess.once('spawn', () => {
+        demuxProbe(stream).then(probe => {
+            let resource = createAudioResource(probe.stream)
+            player.play(resource)
+            connection.subscribe(player)
+        }).catch(err => {
+            if (!streamProcess.killed) process.kill()
+            stream.resume()
+            console.log(err)
+        })
+    })
+    server.queue.shift()
 }
 
 module.exports.serverQueue = serverQueue;
